3d24f7b2258528433056da8ebd7fdcf4c40a2e65
александр захаркин <sanyazaharkin@gmail.com>
2020-04-18 20:07:44 +0500

    вставил систему идентификации

---
+++ FreeStudent/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FreeStudent.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
