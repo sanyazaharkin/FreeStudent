8d80988e3bb5ce58957356910aa970f9425346b5
александр захаркин <sanyazaharkin@gmail.com>
2020-04-23 17:40:15 +0500

    add-migration10

--- FreeStudent/Migrations/20200423123953_Init10.Designer.cs
+++ FreeStudent/Migrations/20200423123953_Init10.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using FreeStudent.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FreeStudent.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200423123953_Init10")]
+    partial class Init10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
